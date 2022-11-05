355a50db468619edf25891ed6cabc6df63933669
JohnDoeee <yandex949@gmail.com>
2022-11-05 18:38:16 +0300

    users

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState } from "react"
 function App() {
 	return (
 		<div className="App">
-			<h1 className="title">just master:</h1>
+			<h1 className="title">users:</h1>
 		</div>
 	)
 }
