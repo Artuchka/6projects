f764d698194639d09107d98d6b93091891c1e90e
JohnDoeee <yandex949@gmail.com>
2022-11-05 15:16:49 +0300

    no animation done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import "./App.css"
+import { useState } from "react"
+import "./App.scss"
 
 function App() {
+	const [open, setOpen] = useState(false)
+
 	return (
 		<div className="App">
-			<div>modal</div>
+			<button onClick={() => setOpen(true)}>🥳open modal🥳</button>
+			{open && (
+				<div className="overlay show">
+					<div className="modal">
+						<div className="close" onClick={() => setOpen(false)}>
+							&times;
+						</div>
+						<div className="info">modal info</div>
+					</div>
+				</div>
+			)}
 		</div>
 	)
 }
