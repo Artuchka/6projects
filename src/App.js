505f19c01bae8f0b62cab117a941c757e88cdb92
JohnDoeee <yandex949@gmail.com>
2022-11-05 15:21:36 +0300

    done smooth anim

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import { useState } from "react"
 import "./App.scss"
 
+function Modal() {}
+
 function App() {
 	const [open, setOpen] = useState(false)
 
 	return (
 		<div className="App">
 			<button onClick={() => setOpen(true)}>🥳open modal🥳</button>
-			{open && (
-				<div className="overlay show">
-					<div className="modal">
-						<div className="close" onClick={() => setOpen(false)}>
-							&times;
-						</div>
-						<div className="info">modal info</div>
+
+			<div className={`overlay ${open ? "show" : ""}`}>
+				<div className="modal">
+					<div className="close" onClick={() => setOpen(false)}>
+						&times;
 					</div>
+					<div className="info">modal info</div>
 				</div>
-			)}
+			</div>
 		</div>
 	)
 }
