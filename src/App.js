10c60e245d168ca0b5044d8c7bcb6c6e919e599f
JohnDoeee <yandex949@gmail.com>
2022-11-05 14:58:17 +0300

    done counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { useState } from "react"
 import "./App.css"
 
 function App() {
+	const [count, setCount] = useState(0)
 	return (
 		<div className="App">
-			<div>counter</div>
+			<h1 className="title">Counter:</h1>
+			<div className="count">{count}</div>
+			<div className="btn-container">
+				<button
+					onClick={() => setCount(count - 1)}
+					className="btn btn--danger"
+				>
+					- subtract
+				</button>
+				<button
+					onClick={() => setCount(count + 1)}
+					className="btn btn--ok"
+				>
+					add +
+				</button>
+			</div>
 		</div>
 	)
 }
