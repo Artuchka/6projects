b9cdb6b8f367ff998eec01e753d389c93fef0024
JohnDoeee <yandex949@gmail.com>
2022-11-05 16:21:25 +0300

    master branch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import { useState } from "react"
-import "./App.css"
 
 function App() {
-	const [count, setCount] = useState(0)
 	return (
 		<div className="App">
-			<h1 className="title">Counter:</h1>
-			<div className="count">{count}</div>
-			<div className="btn-container">
-				<button
-					onClick={() => setCount(count - 1)}
-					className="btn btn--danger"
-				>
-					- subtract
-				</button>
-				<button
-					onClick={() => setCount(count + 1)}
-					className="btn btn--ok"
-				>
-					add +
-				</button>
-			</div>
+			<h1 className="title">just master:</h1>
 		</div>
 	)
 }
